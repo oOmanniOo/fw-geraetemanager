384f71b85386b7f84775933265b86e61cf5b1f0e
oOmanniOo <poker_face@gmx.net>
2025-08-20 19:42:39 +0200

    feat: Modell für Fahrzeug und Geräte erstelle

---
+++ fahrzeuge/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Fahrzeug(models.Model):
+    bezeichnung = models.CharField(max_length=255)
+    kennzeichen = models.CharField(max_length=20)
+    funkrufname = models.CharField(max_length=20)
+    standort = models.CharField(max_length=100)
+    aktiv = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.bezeichnung
+
--- fwmanager/settings.py
+++ fwmanager/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'geraete',
+    'fahrzeuge',
 ]
 
 MIDDLEWARE = [
