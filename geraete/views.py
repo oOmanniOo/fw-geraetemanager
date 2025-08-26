7248326dfcf538ba3fb262012e3221fa47a8c747
oOmanniOo <poker_face@gmx.net>
2025-08-26 16:12:06 +0200

    feat: Übersicht der Prüfungen im Gerät hinztugefügt

--- geraete/views.py
+++ geraete/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from . models import Geraet
+from pruefungen.models import Pruefung
 
 class GeraetListView(ListView):
     model = Geraet
@@ -9,21 +10,28 @@ class GeraetListView(ListView):
 
 class GeraetDetailView(DetailView):
     model = Geraet
-    template_name = "geraete/geraet_detail.html"
+    template_name = 'geraete/geraet_detail.html'
+    context_object_name = 'geraet'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pruefungen'] = Pruefung.objects.filter(geraet=self.object).order_by('-datum')   
+        return context
+        
 
 class GeraetCreateView(CreateView):
     model = Geraet
     template_name = "geraete/geraet_form.html"
     fields = ["bezeichnung", "identifikation", "seriennummer", "kategorie", "status", "fahrzeug", "bemerkung"]
-    success_url = reverse_lazy("geraete_liste")
+    success_url = reverse_lazy("geraete:geraete_liste")
 
 class GeraetUpdateView(UpdateView):
     model = Geraet
     template_name = "geraete/geraet_form.html"
     fields = ["bezeichnung", "identifikation", "seriennummer", "kategorie", "status", "fahrzeug", "bemerkung"]
-    success_url = reverse_lazy("geraete_liste")
+    success_url = reverse_lazy("geraete:geraete_liste")
 
 class GeraetDeleteView(DeleteView):
     model = Geraet
     template_name = "geraete/geraet_bestaetigen_loeschen.html"
-    success_url = reverse_lazy("geraete_liste")
+    success_url = reverse_lazy("geraete:geraete_liste")
